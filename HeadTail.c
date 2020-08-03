5b78322f84ecdd01e121f629979b843cfc4f42d7
s1270106 <s1270106@std6dc15.u-aizu.ac.jp>
2020-08-03 11:17:37 +0900

    HoT: Initial revision

---
+++ HeadTail.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+  int i,num,hcount=0,tcount=0;
+  srand((unsigned int)time(NULL));
+  printf("Tossing a coin...\n");
+  for(i=0;i<3;i++){
+    num=rand()%2;
+    printf("Round %d: ",i+1);
+    if(num==0){
+      printf("Heads\n");
+      hcount++;
+    }
+    else{
+      printf("Tails\n");
+      tcount++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",hcount,tcount);
+  return 0;
+}
